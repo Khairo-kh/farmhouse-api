73936a5fcadc63e11453dfc28f7060290de469b7
Khairo Khatib <khairo.kh@outlook.com>
2021-08-30 16:23:02 -0400

    finalized authentication process and routes

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,12 +1,47 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const ApiError = require('../utils/apiError');
+
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+
+  const token = signToken(newUser._id);
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ApiError('Please provide email and password!', 400));
+  }
+
+  const user = await User.findOne({ email }).select('+password');
+  if (!user || !(await user.isCorrectPassword(password, user.password))) {
+    return next(new ApiError('Email and/or password do not match!', 401));
+  }
+
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, 'Please provide you name.'] },
@@ -15,12 +16,34 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please provide a password'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm the password.'],
+    validate: {
+      validator: function (passwd) {
+        return passwd === this.password;
+      },
+      message: "Passwords don't match!",
+    },
   },
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
+
+userSchema.methods.isCorrectPassword = async function (
+  receivedPass,
+  userPassword
+) {
+  return await bcrypt.compare(receivedPass, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
