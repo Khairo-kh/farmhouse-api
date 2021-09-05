8e5fac3438cba7de9b421bbcea18a2b732a4cefe
Khairo Khatib <khairo.kh@outlook.com>
2021-09-05 16:04:18 -0400

    implemented update/delete current user account

--- models/userModel.js
+++ models/userModel.js
@@ -32,6 +32,7 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: Date,
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: { type: Boolean, default: true, select: false },
 });
 
 userSchema.pre('save', async function (next) {
@@ -47,6 +48,11 @@ userSchema.pre('save', function (next) {
     return next();
   }
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
   next();
 });
 
