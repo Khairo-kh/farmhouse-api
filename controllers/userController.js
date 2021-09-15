b75e8f039f7daa0376a49319ed3205453ada022d
Khairo Khatib <khairo.kh@outlook.com>
2021-09-15 16:30:21 -0400

    added /me endpoint to get current logged in user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -39,6 +39,11 @@ exports.deleteMe = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getMe = (req, res, next) => {
+  req.params.id = req.user.id;
+  next();
+};
+
 exports.createUser = (req, res) => {
   res.status(500).json({
     status: 'error',
